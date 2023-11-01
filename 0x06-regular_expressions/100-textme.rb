9cb0a0bafacf7655f5af48f405a4f60680751c29
Aaron Pius <piuschbz@gmail.com>
2023-11-01 11:42:18 +0000

    #advance

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:([A-Za-z]+|\+?\d{11})\] \[to:(\+?\d{11}|[A-za-z]+)\] \[flags:[\W+\d]+\]/).join(',')
+puts ARGV[0].scan(/\[from:([A-Za-z]+|\+?\d{11})\] \[to:(\+?\d{11}|[A-za-z]+)\] \[flags:([\W+\d]+\])/).join(',')

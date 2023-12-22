ffbf4b869264e984d1cee737f35f72ed5ac90b52
Aaron Pius <piuschbz@gmail.com>
2023-12-22 20:31:00 +0000

    add: CM provision script to create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# creates file 'school' in tmp dir
+
+file {'/tmp/school':
+  path => '/tmp/school',
+  ensure => present,
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,2 @@
+# Configuration Management
+This folder contain files/answers to configuration management with Puppet tasks

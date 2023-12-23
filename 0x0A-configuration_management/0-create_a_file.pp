c406d4c06992fe3a82bdd355c3a3b2caaf0f5eae
Aaron Pius <piuschbz@gmail.com>
2023-12-23 22:09:04 +0000

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,9 +3,9 @@
 file {'/tmp/school':
   ensure  => present,
   path    => '/tmp/school',
-  mode    => '0744'
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
-  target  => '/tmp/school'
+  target  => '/tmp/school',
 }

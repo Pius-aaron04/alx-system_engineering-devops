580c95e3e4db08f50a366e66cba804330341a192
Aaron Pius <piuschbz@gmail.com>
2023-12-23 22:15:44 +0000

    let's see

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,10 @@
 # creates file 'school' in tmp dir
 
 file {'/tmp/school':
-  ensure  => present,
+  ensure  => file,
   path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
-  target  => '/tmp/school',
 }

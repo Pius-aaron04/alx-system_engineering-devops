6c66bf766f4808cc3b9e4f7c57876125d457bda3
Pius Aaron <piuschbz@gmail.com>
2024-01-05 23:30:08 +0100

    I go again

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,31 +3,41 @@
 $nginx_config=@(EOF)
 server{
     listen 80 default_server;
-    root /var/www//html;
     index index.html;
     rewrite ^/redirect_me https://github.com/pius-aaron04 permanent;
+    error_page 404 /404.html;
+    location = /404.html{
+        root /var/www/html/404.html;
+        internal;
+    }
 }
 EOF
 class webserver::nginx{
 
   package{'nginx':
     ensure   => installed,
-    provider => "apt",
+    provider => 'apt',
   }
 
   file{ '/var/www/html/index.html':
     ensure   => present,
-    content  => "Hello World!",
+    content  => 'Hello World!',
+  }
+
+  file { '/var/www/html/404.html':
+    ensure   => present,
+    content  => "Ceci n'est pas une page"
   }
 
   file { '/etc/nginx/sites-available/default.bk':
     ensure  => present,
     source  => File['/etc/nginx/sites-available/default']
   }
+
   file { '/etc/nginx/sites-available/default':
     ensure   => present,
     notify   => Service['nginx'],
-    content  => $nginx_config,
+    content  => "$nginx_config",
   }
 
   service{ 'nginx':

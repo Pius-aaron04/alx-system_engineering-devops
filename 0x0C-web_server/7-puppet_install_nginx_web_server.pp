b2aab7e6fe19a870b6e22f5147bdadec86d48ad1
Pius Aaron <piuschbz@gmail.com>
2024-01-05 23:48:25 +0100

    Done

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,47 +1,48 @@
 # Manifest Installs and configures Nginx Web server
 
-$nginx_config=@(EOF)
-server{
+$nginx_config = @(EOF)
+server {
     listen 80 default_server;
-    index /index.html;
+    index index.html;
     rewrite ^/redirect_me https://github.com/pius-aaron04 permanent;
     error_page 404 /404.html;
-    location = /404.html{
-        root /var/www/html/404.html;
+    location = /404.html {
+        root /var/www/html;
         internal;
     }
 }
 EOF
-class webserver::nginx{
 
-  package{'nginx':
+class webserver::nginx {
+
+  package { 'nginx':
     ensure   => installed,
     provider => 'apt',
   }
 
-  file{ '/var/www/html/index.html':
+  file { '/var/www/html/index.html':
     ensure   => present,
     content  => 'Hello World!',
   }
 
   file { '/var/www/html/404.html':
     ensure   => present,
-    content  => "Ceci n'est pas une page"
+    content  => "Ceci n'est pas une page",
   }
 
   file { '/etc/nginx/sites-available/default.bk':
     ensure  => present,
-    source  => File['/etc/nginx/sites-available/default']
+    source  => '/etc/nginx/sites-available/default',
   }
 
   file { '/etc/nginx/sites-available/default':
     ensure   => present,
     notify   => Service['nginx'],
-    mode     => '644',
-    content  => "$nginx_config",
+    mode     => '0644',
+    content  => $nginx_config,
   }
 
-  service{ 'nginx':
+  service { 'nginx':
     ensure   => running,
     enable   => true,
   }

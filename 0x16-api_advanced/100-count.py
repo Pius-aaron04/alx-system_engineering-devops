5b71efb0e86c9c55039dba20f7594eb9b1619440
Pius Aaron <piuschbz@gmail.com>
2024-05-10 17:46:52 +0100

    fixed: output error and pycodestyle

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -16,16 +16,15 @@ def count_words(subreddit, word_list, after=None, words_count={}, count=0):
 
     if not word_list:
         return
-    
+
     url = "https://api.reddit.com/r/{}/hot".format(subreddit)
     params = {"after": after, 'count': count}
     headers = {"User-Agent": "Google Chrome Version 122.0.6261.95"}
     response = requests.get(url, params=params, headers=headers,
                             allow_redirects=False)
-    #unique words
     # frequency of each word in word list
-    frequency = {word.lower(): word_list.count(word) for word in word_list if word.lower() == word.lower()}
-    print(frequency)
+    frequency = {word.lower(): [w.lower() for w in word_list].
+                 count(word.lower()) for word in word_list}
 
     if response.status_code == 200:
         after = response.json().get('after')
@@ -40,13 +39,16 @@ def count_words(subreddit, word_list, after=None, words_count={}, count=0):
             return recurse(subreddit, word_list, after, words_count, count=10)
         titles = [child['data']['title'] for child in data]
         for title in titles:
-            print(title)
             for word in words_count:
                 if word in title.lower():
                     occurence = title.lower().split().count(word)
                     if occurence:
-                        words_count[word] += frequency[word] * occurence;
-        print(words_count)
+                        words_count[word] += frequency[word] * occurence
+
+        # sortes data in descending order
+        words_count = {k: v for k, v in sorted(words_count.items(),
+                                               key=lambda item: item[1],
+                                               reverse=True)}
         for k, v in words_count.items():
             if v:
                 print(k+':', v)

784221599868fccbcadf1c062e9e08b5180c9648
Pius Aaron <piuschbz@gmail.com>
2024-05-22 09:30:26 +0100

    update: syntax error fixed

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,12 +2,12 @@
 
 exec { 'update-ulimit':
   command => "sed -i 's/15/4096/' /etc/default/nginx",
-  path    => 'usr/local/bin: /bin",
+  path    => 'usr/local/bin:/bin',
   notify  => Exec['nginx-restart'],
 }
 
 exec { 'nginx-restart':
-  command     => "service nginx restart",
-  path        => ['/usr/bin', '/usr/sbin', '/sbin', '/bin'],
+  command     => 'service nginx restart',
+  path        => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
   refreshonly => true,
 }
